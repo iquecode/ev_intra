158e9eb4e4c0dd7ed074c6c0ba859fcb09436bd6
Ique <iquecode@gmail.com>
2021-03-01 14:46:30 -0300

    controle versão style.css -> força carregamento sempre que arquivo é alterado

--- index.php
+++ index.php
@@ -11,7 +11,9 @@ require_once 'classes/Config.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Área Ecovileir@</title>
-    <link rel="stylesheet" href="css/style.css">      
+    <!-- <link rel="stylesheet" href="css/style.css">       -->
+    <link rel="stylesheet" href="css/style.css?v=<?= filemtime('css/style.css'); ?>">
+
 </head>
 
 <body>
