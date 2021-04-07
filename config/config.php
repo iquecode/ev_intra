a10aca1445733caf49839a077ea96335f6b25ed1
Ique <iquecode@gmail.com>
2021-04-06 21:08:37 -0300

    implementacao lancamentos em lote - ipca teste ok

--- config/config.php
+++ config/config.php
@@ -3,6 +3,6 @@ define('DB_NAME', 'ev_financeiro');
 define('DB_HOST', 'localhost');
 define('DB_USER','root');
 define('DB_PASS', '');
-define('VERSION', '1.0.3');
+define('VERSION', '1.0.4');
 define('YEAR', '2021');
 define('DIR_BASE', '/ev_intra/');
